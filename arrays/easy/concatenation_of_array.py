4653b43c4943a2c90798f9db24bb9fd0fb783a2b
danilagorbacev <mikenaon35902@gmail.com>
2026-02-22 13:33:18 +0300

    resolve easy problem of array

---
+++ arrays/easy/concatenation_of_array.py
@@ -0,0 +1,15 @@
+"""
+Problem: Concatenation of array
+Difficulty: Easy
+Topic: Array
+Link: https://leetcode.com/problems/concatenation-of-array/description/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        nums.extend(nums)
+        return nums
